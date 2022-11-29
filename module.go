fbcc07f2bd5194eefdffc68093fd9daa2e46a03a
MEZ <mez@bitwormhole.com>
2022-11-29 17:16:12 +0800

    update to v0.0.3

--- module.go
+++ module.go
@@ -2,6 +2,6 @@ package afs
 
 const (
 	theModuleName     = "bitwormhole.com/starter/afs"
-	theModuleVersion  = "v0.0.2"
-	theModuleRevision = 2
+	theModuleVersion  = "v0.0.3"
+	theModuleRevision = 3
 )
