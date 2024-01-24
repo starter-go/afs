99e0be9f53b31306e50b84738bdef03f96951419
MEZ <mez@bitwormhole.com>
2024-01-24 20:51:52 +0800

    up to v0.9.5

--- module.go
+++ module.go
@@ -2,6 +2,6 @@ package afs
 
 const (
 	theModuleName     = "github.com/starter-go/afs"
-	theModuleVersion  = "v0.9.4"
-	theModuleRevision = 12
+	theModuleVersion  = "v0.9.5"
+	theModuleRevision = 13
 )
