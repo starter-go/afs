54d66d47ca9cc89ada5ea44bfd390573985a452e
xukun <xukun17@sina.com>
2024-07-31 20:14:47 +0800

    up to v0.9.9

--- module.go
+++ module.go
@@ -2,6 +2,6 @@ package afs
 
 const (
 	theModuleName     = "github.com/starter-go/afs"
-	theModuleVersion  = "v0.9.8"
-	theModuleRevision = 16
+	theModuleVersion  = "v0.9.9"
+	theModuleRevision = 17
 )
